d7f8f33f66a7f041ab76db3f3061a8d89823c46c
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2019-10-11 11:53:14 +0200

    initial state and goals extraction using xml parser

--- main.py
+++ main.py
@@ -6,7 +6,4 @@ if __name__ == '__main__':
     # file = sys.argv[1]
     file = "plan_pfile-83-1-2--I8-G6-n30.pddl_1.SOL" #for debugging, use command line arguments
     p = plan.Plan(file)
-    # now init contains the clean xml of the init part,
-    # actions is an array that contains the clean xml of all the actions,
-    # goals contains the clean xml of the goals part
     print("test")
--- plan.py
+++ plan.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xml.etree.ElementTree as ET
 
 
 class Plan:
@@ -6,6 +7,10 @@ class Plan:
         f = open(plan_description, "r")
         lines = [line for line in f.readlines()]
         data = [l.split(";;(:metadata")[1] for l in lines if l.find(";;(:metadata") >= 0]
-        self.init = data[0]
+        init_root = ET.fromstring(data[0])
+        goals_root = ET.fromstring(data[len(data) - 1])
+        self.initial_state = [child.text for child in init_root]
         self.actions = [act[:-2] for act in data[1:-1]]
-        self.goals = data[len(data) - 1]
+        self.goals = [child.text for child in goals_root]
+
+
