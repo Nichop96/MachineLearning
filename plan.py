16768a6e76d368add2da0aae04b114ad82de3855
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2019-10-17 17:05:55 +0200

    Primo punto fatto

--- plan.py
+++ plan.py
@@ -10,8 +10,8 @@ class Plan:
         data = [l.split(";;(:metadata")[1] for l in lines if l.find(";;(:metadata") >= 0]
         init_root = ET.fromstring(data[0])
         goals_root = ET.fromstring(data[len(data) - 1])
-        self.initial_state = [child.text for child in init_root]
+        self.initial_state = [child.text[1:-1] for child in init_root]
         act = [act[:-2] for act in data[1:-1]]
-        self.goals = [child.text for child in goals_root]
+        self.goals = [child.text[1:-1] for child in goals_root]
         self.actions = [action.Action(a) for a in act]
 
