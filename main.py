8535afcd6805bf7ea4fe45ed45db551ab0ae9739
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2019-10-17 17:23:27 +0200

    Iterazione Cartella

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import numpy as np
 import plan
 import action
+import sys
+import os
 
 
 if __name__ == '__main__':
     # file = sys.argv[1]
-    file = "plan_pfile-83-1-2--I8-G6-n30.pddl_1.SOL" #for debugging, use command line arguments
-    p = plan.Plan(file)
+    # file = "plan_pfile-51-0-1--I0-G0-n0.pddl_1.SOL" #for debugging, use command line arguments
+    # p = plan.Plan(file)
+    folder = "SOL_files"
+    plan_list = [plan.Plan(folder + "/" + file) for file in os.listdir("SOL_files") if file.endswith(".SOL")]
     print("test")
