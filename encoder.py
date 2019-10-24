f346a8cacad52d2dc513b8ffd9c0d17676fcc7d8
MatteoRocco11 <52277934+MatteoRocco11@users.noreply.github.com>
2019-10-24 17:08:29 +0200

    update 2

--- encoder.py
+++ encoder.py
@@ -6,26 +6,26 @@ import numpy as np
 def encoder(domains_list):
         apn_list = []
         for dom in domains_list:
-            apn_list = apn_list + dom.apn
-        apn_list = np.asanyarray(list(set(apn_list)))
+            apn_list = apn_list + [element.upper() for element in dom.apn]
+        apn_list = list(set(apn_list))
         save_arrays.save(apn_list, "apn.obj")
         cit_list = []
         for dom in domains_list:
-            cit_list = cit_list + dom.apn
-        cit_list = np.asanyarray(list(set(cit_list)))
+            cit_list = cit_list + [element.upper() for element in dom.cit]
+        cit_list = list(set(cit_list))
         save_arrays.save(cit_list, "cit.obj")
         loc_list = []
         for dom in domains_list:
-            loc_list = loc_list + dom.apn
-        loc_list = np.asanyarray(list(set(loc_list)))
+            loc_list = loc_list + [element.upper() for element in dom.loc]
+        loc_list = list(set(loc_list))
         save_arrays.save(loc_list, "loc.obj")
         obj_list = []
         for dom in domains_list:
-            obj_list = obj_list + dom.apn
-        obj_list = np.asanyarray(list(set(obj_list)))
+            obj_list = obj_list + [element.upper() for element in dom.obj]
+        obj_list = list(set(obj_list))
         save_arrays.save(obj_list, "obj.obj")
         tru_list = []
         for dom in domains_list:
-            tru_list = tru_list + dom.apn
-        tru_list = np.asanyarray(list(set(tru_list)))
+            tru_list = tru_list + [element.upper() for element in dom.tru]
+        tru_list = list(set(tru_list))
         save_arrays.save(tru_list, "tru.obj")
