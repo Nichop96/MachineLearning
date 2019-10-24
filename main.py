4b2e81520fc473531b0e8b4156ca54f44a086fd2
Nichop96 <48830373+Nichop96@users.noreply.github.com>
2019-10-24 17:57:10 +0200

    awetgs

--- main.py
+++ main.py
@@ -4,21 +4,10 @@ import numpy as np
 import encoder
 import save_arrays
 import pickle
+import oneHot
+import natsort as na
 
 if __name__ == '__main__':
-    # file = sys.argv[1]
-    # file = "plan_pfile-51-0-1--I0-G0-n0.pddl_1.SOL" #for debugging, use command line arguments
-    # p = plan.Plan(file)
-    # folder = "C:\\Users\\user\\Downloads\\logistics\\cb_problems"
-    # #folder = "SOL_files"
-    # print("inizio")
-    # #plans = utils.get_plans(folder)
-    # domains = utils.logistics_domains(folder)
-    # #statistics.init_statistics(domains)
-    # print("OK")
-    # #ave_arrays.save(domains, "domains.obj")
-    # encoder.encoder(domains)
-    # print("fine")
     file1 = open("apn.obj", "rb")
     apn_list = pickle.load(file1)[1:]
     file2 = open("cit.obj", "rb")
@@ -29,4 +18,20 @@ if __name__ == '__main__':
     loc_list = pickle.load(file4)
     file5 = open("tru.obj", "rb")
     tru_list = pickle.load(file5)
-    print("ok")
+
+    apn_list = na.natsorted(apn_list[1:])
+    cit_list = na.natsorted(cit_list)
+    obj_list = na.natsorted(obj_list[1:])
+    tru_list = na.natsorted(tru_list)
+    loc_list = na.natsorted(loc_list)
+
+    folder = "SOL_files"
+    print("inizio")
+    plans = utils.get_plans(folder)
+    # domains = utils.logistics_domains(folder)
+    # statistics.init_statistics(domains)
+    print("OK")
+    # ave_arrays.save(domains, "domains.obj")
+    # encoder.encoder(domains)
+    oneHot.init(plans, apn_list, cit_list, obj_list, loc_list, tru_list)
+    print("fine")
