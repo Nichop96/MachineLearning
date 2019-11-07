d7f96d341789e03a6588c2ccd23f831b4e1d93ef
kernelMaster47 <l.bonassi005@studenti.unibs.it>
2019-11-07 13:40:57 +0100

    order

--- crea_istanze.py
+++ crea_istanze.py
@@ -4,5 +4,15 @@ import numpy as np
 
 def crea(plans):
     actions = np.concatenate([p.actions for p in plans], axis=0)
-    db = [(action.oneHotprecondition, action.oneHotAtction) for action in actions]
+    db = []
+    for action in actions:
+        tot_sum = np.zeros(len(action.oneHotprecondition[0]))
+        for prec in action.oneHotprecondition:
+            tot_sum = np.add(tot_sum, prec)
+        for pos in action.oneHotpositiveEffects:
+            tot_sum = np.add(tot_sum, pos)
+        for neg in action.oneHotnegativeEffects:
+            tot_sum = np.add(tot_sum, neg)
+        tot_sum = tot_sum / (len(action.oneHotprecondition) + len(action.oneHotpositiveEffects) + len(action.oneHotnegativeEffects))
+        db.append((tot_sum, action.oneHotAtction))
     return db
